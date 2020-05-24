df4830806ae3d9c72cdfa1260aabf85ff04040e5
Anish Thakker <anish.thakker1@gmail.com>
2020-05-24 18:23:33 -0400

    All data imported + able to query
    Able to query data from 2002-2020.

--- cfb-recruiting/app.R
+++ cfb-recruiting/app.R
@@ -8,9 +8,14 @@
 #
 
 library(shiny)
+library(readr)
+library(rvest)
+library(tidyverse)
+library(data.table)
+library(DT)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
+ui <- shinyUI(fluidPage(
 
     # Application title
     titlePanel("Old Faithful Geyser Data"),
@@ -18,32 +23,61 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            selectInput("state", "State:",
+                                    c("MD" = "MD",
+                                      "AL" = "AL",
+                                      "CA" = "CA")),
+            selectInput("pos", "Position:",
+                        c("WR" = "WR",
+                          "RB" = "RB",
+                          "DT" = "DT")),
+            selectInput("yr", "Class:",
+                        c("2002" = "2002",
+                          "2003" = "2003",
+                          "2004" = "2004",
+                          "2005" = "2005",
+                          "2006" = "2006",
+                          "2007" = "2007",
+                          "2008" = "2008",
+                          "2009" = "2009",
+                          "2010" = "2010",
+                          "2011" = "2011",
+                          "2012" = "2012",
+                          "2013" = "2013",
+                          "2014" = "2014",
+                          "2015" = "2015",
+                          "2016" = "2016",
+                          "2017" = "2017",
+                          "2018" = "2018",
+                          "2019" = "2019",
+                          "2020" = "2020"))
         ),
+    
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+            #DT::dataTableOutput("recruits")
+           textOutput("txtOutput")
         )
     )
 )
+)
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+server <- shinyServer(function(input, output) {
+    output$txtOutput = renderText({
+        recruits<-reactive({
+            csv_add<-paste(input$yr,".csv",sep = "")
+            str<-paste("data/", csv_add, sep="")
+            read_csv((str)) %>%
+                filter(stateProvince==input$state) %>% 
+                filter(position==input$pos)
+            
+        })
+        paste("The area of the circle is: ", recruits()[1,4])
     })
-}
+    
+})
 
 # Run the application 
 shinyApp(ui = ui, server = server)
